f362c0a6c35e8ecb500568030fc768f6daba6fcb
Ishan Khanna <ishan1604@gmail.com>
2016-02-01 02:20:07 +0530

    Now able to dismiss notiications

---
+++ NotifShredder/app/src/main/java/in/ishankhanna/notifshredder/utils/NotificationListener.java
@@ -0,0 +1,32 @@
+package in.ishankhanna.notifshredder.utils;
+
+import android.service.notification.NotificationListenerService;
+import android.service.notification.StatusBarNotification;
+import android.util.Log;
+
+/**
+ * Created by ishan on 01/02/16.
+ */
+public class NotificationListener extends NotificationListenerService {
+
+    SharedPreferenceHelper sharedPreferenceHelper;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        sharedPreferenceHelper = SharedPreferenceHelper.getInstance(getApplicationContext());
+    }
+
+    @Override
+    public void onNotificationPosted(StatusBarNotification sbn) {
+        super.onNotificationPosted(sbn);
+
+        String packageName = sbn.getPackageName();
+        Log.d("ShredderService", packageName);
+        if (sharedPreferenceHelper.isBlocked(packageName)) {
+            cancelNotification(sbn.getKey());
+            Log.d("ShredderService", packageName + "will be canceled");
+        }
+
+    }
+}
